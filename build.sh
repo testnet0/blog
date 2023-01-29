d9b4d4f0a649b434f5695da437df2a348b08b1fa
TestNet <1@shengkai.wang>
2023-01-29 14:54:00 +0800

    更新脚本

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 current_dir=$(pwd)
 
+function rm_docker {
+  docker-compose rm -f
+  docker image rm web:1.0
+}
+#检测环境
+function check_environment {
 echo "================================"
 echo "开始检测环境"
 
@@ -11,6 +17,7 @@ else
 echo "mvn命令不存在，请先安装Maven"
 exit
 fi
+
 # 检测nodejs环境是否存在
 if command -v npm >/dev/null 2>&1 ; then
 echo "npm is installed"
@@ -26,8 +33,10 @@ else
 echo "docker-compose命令不存在，请先安装docker-compose！"
 exit
 fi
+}
 
 # 配置Mysql密码
+function configure_mysql {
 echo "请输入Mysql密码："
 read password
 if [ -z "$password" ]; then
@@ -36,9 +45,11 @@ if [ -z "$password" ]; then
 fi
 sed -i "s/<mysql_password>/$password/g" $current_dir"/blog-springboot/src/main/resources/application.yml"
 sed -i "s/<mysql_password>/$password/g" $current_dir"/docker-compose.yaml"
+}
+
 
 # 配置域名信息
-
+function configure_host {
 echo "请输入你自己的域名："
 read domain
 if [ -z "$domain" ]; then
@@ -48,9 +59,10 @@ fi
 sed -i "s/<domain>/$domain/g" $current_dir"/blog-springboot/src/main/resources/application.yml"
 sed -i "s/<domain>/$domain/g" $current_dir"/nginx.conf"
 
-
+}
 # 配置发送邮件服务器的地址
 
+function configure_mail {
 echo "请输入发信邮箱服务器，如stmp.xx.xx："
 read mail_host
 if [ -z "$mail_host" ]; then
@@ -82,10 +94,13 @@ if [ -z "$mail_port" ]; then
   exit 1
 fi
 sed -i "s/<mail_port>/$mail_port/g" $current_dir"/blog-springboot/src/main/resources/application.yml"
+}
 
-echo "================================"
-echo "开始编译代码"
 # 编译后端代码
+
+function build_backend {
+echo "================================"
+echo "开始编译后端代码"
 cd $current_dir"/blog-springboot"
 mvn clean install
 if [ $? -eq 0 ]; then
@@ -93,19 +108,42 @@ echo "Build Successful"
 else
 echo "Maveb Build Failed"
 fi
+}
 
 # 编译前端代码
+function build_front {
+echo "================================"
+echo "开始编译前端代码"
 cd $current_dir"/blog-vue/admin"
 npm install && npm run build
 cd $current_dir"/blog-vue/blog"
 npm install && npm run build
-echo "================================"
-echo "请去域名控制台，添加域名解析A记录,指向服务器的公网IP："
-echo $domain
-echo "ws."$domain
-echo "admin."$domain
-echo "upload."$domain
-echo "================================"
+}
 
+function echo_end {
+echo "================================"
+echo "编译完成请去域名控制台，添加域名解析A记录,指向服务器的公网IP："
+echo $domain ====>IP
+echo "ws."$domain ====>IP
+echo "admin."$domain ====>IP
+echo "upload."$domain ====>IP
+echo "================================"
+}
+
+if ls -d $current_dir/data &> /dev/null; then
+  echo "检测到已经安装，进入更新模式"
+  rm_docker
+  build_backend
+  build_front
+else
+  echo "检测到未安装，进入配置模式"
+  check_environment
+  configure_mysql
+  configure_host
+  configure_mail
+  build_backend
+  build_front
+  echo_end
+fi
 
 docker-compose up
