4576d2a5b31d3799d24a8e24d1acfc3e3b7b893a
TestNet <1@shengkai.wang>
2023-01-30 13:00:34 +0800

    增加天气信息

--- blog-vue/blog/src/views/home/Home.vue
+++ blog-vue/blog/src/views/home/Home.vue
@@ -13,28 +13,15 @@
         </div>
         <!-- 联系方式 -->
         <div class="blog-contact">
-          <a
-            v-if="isShowSocial('qq')"
-            class="mr-5 iconfont iconqq"
-            target="_blank"
-            :href="
-              'http://wpa.qq.com/msgrd?v=3&uin=' +
-                blogInfo.websiteConfig.qq +
-                '&site=qq&menu=yes'
-            "
-          />
-          <a
-            v-if="isShowSocial('github')"
-            target="_blank"
-            :href="blogInfo.websiteConfig.github"
-            class="mr-5 iconfont icongithub"
-          />
-          <a
-            v-if="isShowSocial('gitee')"
-            target="_blank"
-            :href="blogInfo.websiteConfig.gitee"
-            class="iconfont icongitee-fill-round"
-          />
+          <a v-if="isShowSocial('qq')" class="mr-5 iconfont iconqq" target="_blank" :href="
+            'http://wpa.qq.com/msgrd?v=3&uin=' +
+            blogInfo.websiteConfig.qq +
+            '&site=qq&menu=yes'
+          " />
+          <a v-if="isShowSocial('github')" target="_blank" :href="blogInfo.websiteConfig.github"
+            class="mr-5 iconfont icongithub" />
+          <a v-if="isShowSocial('gitee')" target="_blank" :href="blogInfo.websiteConfig.gitee"
+            class="iconfont icongitee-fill-round" />
         </div>
       </div>
       <!-- 向下滚动 -->
@@ -51,21 +38,12 @@
         <v-card class="animated zoomIn" v-if="talkList.length > 0">
           <Swiper :list="talkList" />
         </v-card>
-        <v-card
-          class="animated zoomIn article-card"
-          style="border-radius: 12px 8px 8px 12px"
-          v-for="(item, index) of articleList"
-          :key="item.id"
-        >
+        <v-card class="animated zoomIn article-card" style="border-radius: 12px 8px 8px 12px"
+          v-for="(item, index) of articleList" :key="item.id">
           <!-- 文章封面图 -->
           <div :class="isRight(index)">
             <router-link :to="'/articles/' + item.id">
-              <v-img
-                class="on-hover"
-                width="100%"
-                height="100%"
-                :src="item.articleCover"
-              />
+              <v-img class="on-hover" width="100%" height="100%" :src="item.articleCover" />
             </router-link>
           </div>
           <!-- 文章信息 -->
@@ -94,13 +72,8 @@
               </router-link>
               <span class="separator">|</span>
               <!-- 文章标签 -->
-              <router-link
-                style="display:inline-block"
-                :to="'/tags/' + tag.id"
-                class="mr-1"
-                v-for="tag of item.tagDTOList"
-                :key="tag.id"
-              >
+              <router-link style="display:inline-block" :to="'/tags/' + tag.id" class="mr-1"
+                v-for="tag of item.tagDTOList" :key="tag.id">
                 <v-icon size="14">mdi-tag-multiple</v-icon>{{ tag.tagName }}
               </router-link>
             </div>
@@ -122,10 +95,7 @@
             <div class="author-wrapper">
               <!-- 博主头像 -->
               <v-avatar size="110">
-                <img
-                  class="author-avatar"
-                  :src="blogInfo.websiteConfig.websiteAvatar"
-                />
+                <img class="author-avatar" :src="blogInfo.websiteConfig.websiteAvatar" />
               </v-avatar>
               <div style="font-size: 1.375rem;margin-top:0.625rem">
                 {{ blogInfo.websiteConfig.websiteAuthor }}
@@ -166,29 +136,19 @@
             </a>
             <!-- 社交信息 -->
             <div class="card-info-social">
-              <a
-                v-if="isShowSocial('qq')"
-                class="mr-5 iconfont iconqq"
-                target="_blank"
-                :href="
-                  'http://wpa.qq.com/msgrd?v=3&uin=' +
-                    blogInfo.websiteConfig.qq +
-                    '&site=qq&menu=yes'
-                "
-              />
-              <a
-                v-if="isShowSocial('github')"
-                target="_blank"
-                :href="blogInfo.websiteConfig.github"
-                class="mr-5 iconfont icongithub"
-              />
-              <a
-                v-if="isShowSocial('gitee')"
-                target="_blank"
-                :href="blogInfo.websiteConfig.gitee"
-                class="iconfont icongitee-fill-round"
-              />
+              <a v-if="isShowSocial('qq')" class="mr-5 iconfont iconqq" target="_blank" :href="
+                'http://wpa.qq.com/msgrd?v=3&uin=' +
+                blogInfo.websiteConfig.qq +
+                '&site=qq&menu=yes'
+              " />
+              <a v-if="isShowSocial('github')" target="_blank" :href="blogInfo.websiteConfig.github"
+                class="mr-5 iconfont icongithub" />
+              <a v-if="isShowSocial('gitee')" target="_blank" :href="blogInfo.websiteConfig.gitee"
+                class="iconfont icongitee-fill-round" />
             </div>
+          </v-card>
+          <v-card class="blog-card animated zoomIn mt-5">
+            <div id="he-plugin-standard"></div>
           </v-card>
           <!-- 网站信息 -->
           <v-card class="blog-card animated zoomIn mt-5 big">
@@ -239,7 +199,24 @@ export default {
     this.listHomeTalks();
     this.timer = setInterval(this.runTime, 1000);
   },
-  data: function() {
+  mounted: function () {
+    window.WIDGET = {
+      "CONFIG": {
+        "layout": "2",
+        "width": 230,
+        "height": 300,
+        "background": "2",
+        "dataColor": "000000",
+        "borderRadius": "5",
+        "key": "4034255d0cc94897b5f745367ccb81f2"
+      }
+    };
+    let script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "https://widget.qweather.net/standard/static/js/he-standard-common.js?v=2.0";
+    document.getElementsByTagName("head")[0].appendChild(script);
+  },
+  data: function () {
     return {
       tip: false,
       time: "",
@@ -330,7 +307,7 @@ export default {
   },
   computed: {
     isRight() {
-      return function(index) {
+      return function (index) {
         if (index % 2 == 0) {
           return "article-cover left-radius";
         }
@@ -341,7 +318,7 @@ export default {
       return this.$store.state.blogInfo;
     },
     isShowSocial() {
-      return function(social) {
+      return function (social) {
         return this.blogInfo.websiteConfig.socialUrlList.indexOf(social) != -1;
       };
     },
@@ -383,48 +360,60 @@ export default {
   color: #fff !important;
   animation: header-effect 1s;
 }
+
 .banner-container {
   margin-top: 43vh;
   line-height: 1.5;
   color: #eee;
 }
+
 .blog-contact a {
   color: #fff !important;
 }
+
 .card-info-social {
   line-height: 40px;
   text-align: center;
   margin: 6px 0 -6px;
 }
+
 .card-info-social a {
   font-size: 1.5rem;
 }
+
 .left-radius {
   border-radius: 8px 0 0 8px !important;
   order: 0;
 }
+
 .right-radius {
   border-radius: 0 8px 8px 0 !important;
   order: 1;
 }
+
 .article-wrapper {
   font-size: 14px;
 }
+
 @media (min-width: 760px) {
   .blog-title {
     font-size: 2.5rem;
   }
+
   .blog-intro {
     font-size: 1.5rem;
   }
+
   .blog-contact {
     display: none;
   }
+
   .home-container {
     max-width: 1200px;
     margin: calc(100vh - 48px) auto 28px auto;
     padding: 0 5px;
   }
+
   .article-card {
     display: flex;
     align-items: center;
@@ -432,79 +421,98 @@ export default {
     width: 100%;
     margin-top: 20px;
   }
+
   .article-cover {
     overflow: hidden;
     height: 100%;
     width: 45%;
   }
+
   .on-hover {
     transition: all 0.6s;
   }
+
   .article-card:hover .on-hover {
     transform: scale(1.1);
   }
+
   .article-wrapper {
     padding: 0 2.5rem;
     width: 55%;
   }
+
   .article-wrapper a {
     font-size: 1.5rem;
     transition: all 0.3s;
   }
 }
+
 @media (max-width: 759px) {
   .blog-title {
     font-size: 26px;
   }
+
   .blog-contact {
     font-size: 1.25rem;
     line-height: 2;
   }
+
   .home-container {
     width: 100%;
     margin: calc(100vh - 66px) auto 0 auto;
   }
+
   .article-card {
     margin-top: 1rem;
   }
+
   .article-cover {
     border-radius: 8px 8px 0 0 !important;
     height: 230px !important;
     width: 100%;
   }
+
   .article-cover div {
     border-radius: 8px 8px 0 0 !important;
   }
+
   .article-wrapper {
     padding: 1.25rem 1.25rem 1.875rem;
   }
+
   .article-wrapper a {
     font-size: 1.25rem;
     transition: all 0.3s;
   }
 }
+
 .scroll-down {
   cursor: pointer;
   position: absolute;
   bottom: 0;
   width: 100%;
 }
+
 .scroll-down i {
   font-size: 2rem;
 }
+
 .article-wrapper a:hover {
   color: #8e8cd8;
 }
+
 .article-info {
   font-size: 95%;
   color: #858585;
   line-height: 2;
   margin: 0.375rem 0;
 }
+
 .article-info a {
   font-size: 95%;
   color: #858585 !important;
 }
+
 .article-content {
   line-height: 2;
   overflow: hidden;
@@ -513,29 +521,36 @@ export default {
   -webkit-line-clamp: 3;
   -webkit-box-orient: vertical;
 }
+
 .blog-wrapper {
   position: sticky;
   top: 10px;
 }
+
 .blog-card {
   line-height: 2;
   padding: 1.25rem 1.5rem;
 }
+
 .author-wrapper {
   text-align: center;
 }
+
 .blog-info-wrapper {
   display: flex;
   justify-self: center;
   padding: 0.875rem 0;
 }
+
 .blog-info-data {
   flex: 1;
   text-align: center;
 }
+
 .blog-info-data a {
   text-decoration: none;
 }
+
 .collection-btn {
   text-align: center;
   z-index: 1;
@@ -549,6 +564,7 @@ export default {
   transition-duration: 1s;
   transition-property: color;
 }
+
 .collection-btn:before {
   position: absolute;
   top: 0;
@@ -564,20 +580,25 @@ export default {
   transform: scaleX(0);
   transform-origin: 0 50%;
 }
+
 .collection-btn:hover:before {
   transition-timing-function: cubic-bezier(0.45, 1.64, 0.47, 0.66);
   transform: scaleX(1);
 }
+
 .author-avatar {
   transition: all 0.5s;
 }
+
 .author-avatar:hover {
   transform: rotate(360deg);
 }
+
 .web-info {
   padding: 0.25rem;
   font-size: 0.875rem;
 }
+
 .scroll-down-effects {
   color: #eee !important;
   text-align: center;
@@ -588,32 +609,39 @@ export default {
   -webkit-font-smoothing: antialiased;
   animation: scroll-down-effect 1.5s infinite;
 }
+
 @keyframes scroll-down-effect {
   0% {
     top: 0;
     opacity: 0.4;
     filter: alpha(opacity=40);
   }
+
   50% {
     top: -16px;
     opacity: 1;
     filter: none;
   }
+
   100% {
     top: 0;
     opacity: 0.4;
     filter: alpha(opacity=40);
   }
 }
+
 .big i {
   color: #f00;
   animation: big 0.8s linear infinite;
 }
+
 @keyframes big {
+
   0%,
   100% {
     transform: scale(1);
   }
+
   50% {
     transform: scale(1.2);
   }
